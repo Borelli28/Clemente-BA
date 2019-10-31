1b8ca33b7f0f0bfe205948ba878b4130eef46944
ABorelli <Borelliarmando12@gmail.com>
2019-10-31 13:00:55 -0500

    Initial Commit

---
+++ Clemente Batting Average/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clemente Batting Average
+//
+//  Created by Armando Borelli on 10/31/19.
+//  Copyright © 2019 Armando Borelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
